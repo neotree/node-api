c853399b79b7ee5c9a90baf996e5bddf975e19ca
farai <farai.matare@bwscloud.tech>
2021-03-09 09:29:10 +0200

    Fix the entry file

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 require('dotenv').config({ path: process.env.ENV_FILE || './.env' }); // enviroment variables
 
-// server
-if (process.env.NODE_ENV === 'production') {
-  require('./dist/server');
-} else {
-  require('./server');
-}
+require('./server');
