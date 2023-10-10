8bc9e100b4deb51463e47c9ae94ddc9653b3530f
farai-bws <farai.matare@bwscloud.tech>
2023-10-10 18:29:10 +0200

    sendMail

--- server/mailer.js
+++ server/mailer.js
@@ -1,29 +1,27 @@
 const nodemailer = require('nodemailer');
 const  HTML_TEMPLATE = require("./mail-template.js");
 
-const config = {
-    emailFrom: process.env.MAIL_FROM_ADDRESS,
-    emailsTo: process.env.MAIL_RECEIVERS,
-    emailPassword: process.env.MAIL_PASSWORD,
-};
-
 const transporter = nodemailer.createTransport({
   service: process.env.MAIL_MAILER,
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
   secure: true,
   auth: {
-    user: config.emailFrom,
-    pass: config.emailPassword
+    user: process.env.MAIL_USERNAME,
+    pass: process.env.MAIL_PASSWORD
   }
 });
 
+console.log('transporter', transporter);
+
 const mailOptions = {
-  from: config.emailFrom,
-  to: config.emailsTo,
+  from: process.env.MAIL_FROM_ADDRESS,
+  to: process.env.MAIL_RECEIVERS,
   subject: 'NEOTREE MOBILE APP EXCEPTION',
 };
+
 module.exports = function sendEmail(message,callback) {
+	console.log('sendEmail message = ', message);
 	if(message){
 		transporter.sendMail({ ...mailOptions, html: HTML_TEMPLATE(message) }, function(error, info){
 			console.log('sendEmail', error, info);
